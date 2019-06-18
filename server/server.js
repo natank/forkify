e0b6258461f186c73c55a9cc9ffc1b68f952158c
Nati <natan.kamusher@gmail.com>
2019-06-18 17:35:53 +0300

    Added fetch request handling

--- server/server.js
+++ server/server.js
@@ -19,7 +19,8 @@ app.listen(3000, () => console.log('Example app listening on port 3000'));
 
 
 app.get('/search', function(req, res){
-  fetch('https://www.food2fork.com/api/get?key=a3aa0083a984cf4447b5b313af270582&rId=35382')
+  // fetch('https://www.food2fork.com/api/get?key=a3aa0083a984cf4447b5b313af270582&rId=35382')
+  fetch(req.params.encodedURL)
   .then(result=> result.json())
   .then(json=>res.json(json))
   .catch(error=>console.log(error))

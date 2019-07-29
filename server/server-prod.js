d4784a3cf75478c675479ef108e2a51f848dcbbe
Nati <natan.kamusher@gmail.com>
2019-07-29 13:52:39 +0300

    Added dist folder

--- server/server-prod.js
+++ server/server-prod.js
@@ -14,7 +14,7 @@ const app = express(),
 
 app.use(express.static(DIST_DIR));
 
-app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port 3000. NODE_ENV = ${process.env.NODE_ENV} DIST_DIR = ${__dirname} HTML_FILE = ${HTML_FILE}`));
+app.listen(process.env.PORT || 3000, () => {});
 
 app.get('/search', function (req, res) {
   // https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=2
@@ -22,33 +22,27 @@ app.get('/search', function (req, res) {
   const keyword = req.query.searchString.replace(/ /g, '%20');
   const URL = `https://www.food2fork.com/api/search?key=a3aa0083a984cf4447b5b313af270582&q=${keyword}&page=2`;
 
-  console.log(`URL=${URL}`);
   fetch(URL)
     .then(result => {
       return result.json();
     })
     .then(data => {
-      console.log(data);
       res.send(data);
     })
-    .catch(error => console.log(error));
+    .catch(error => {});
 });
 
 app.get('/recipe', function (req, res) {
   const recipeId = req.query.recipeId;
-  console.log(`req.query: ${JSON.stringify(req.query)}`);
   const URL = `https://www.food2fork.com/api/get?key=a3aa0083a984cf4447b5b313af270582&rId=${recipeId}`;
-  console.log(`req.url: ${req.url}`);
-  console.log(`url: ${URL}`);
   fetch(URL)
     .then(result => {
       return result.json();
     })
     .then(data => {
-      console.log(`data: ${JSON.stringify(data)}`);
       res.send(data);
     })
-    .catch(error => console.log(`unable to fetch data: ${error}`));
+    .catch(error => {});
 });
 
 app.get('/', (req, res) => {
